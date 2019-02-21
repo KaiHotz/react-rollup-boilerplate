1df76c0851359659e269f77a5cdeb2848fdd989f
Kai Hotz <kaihotz@me.com>
2019-02-21 23:07:55 +0100

    react-styleguidist updatet

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 const {
   createConfig, babel, css, sass, setOutput, match, file,
 } = require('webpack-blocks')
@@ -6,18 +8,36 @@ const pkg = require('./package.json')
 
 module.exports = {
   title: `${pkg.name} v${pkg.version}`,
-  components: 'src/lib/components/**/*.{js,jsx,ts,tsx}',
+  components: 'src/lib/components/**/[A-Z]*.js',
+  moduleAliases: {
+    [pkg.name]: path.resolve(__dirname, 'src/lib'),
+  },
+  ribbon: {
+    url: 'https://github.com/KaiHotz/react-rollup-boilerplate',
+    text: 'Fork me on GitHub',
+  },
   showSidebar: true,
-  usageMode: 'collapse',
+  usageMode: 'expand',
   skipComponentsWithoutExample: true,
   theme: {
     color: {
-      link: '#1978c8',
+      link: '#065fd4',
       linkHover: '#00adef',
     },
     font: ['Helvetica', 'sans-serif'],
   },
   styles: {
+    Ribbon: {
+      root: {
+        backgroundImage: 'url("https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png")',
+        backgroundSize: '50px 50px',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'right top',
+      },
+      link: {
+        backgroundColor: '#065fd4',
+      },
+    },
     Heading: {
       heading2: {
         fontSize: 26,
@@ -50,4 +70,13 @@ module.exports = {
   getExampleFilename(componentPath) {
     return componentPath.replace(/\.js?$/, '.examples.md')
   },
+  getComponentPathLine(componentPath) {
+    const name = path.basename(componentPath, '.js')
+
+    return `import { ${name} } from ${pkg.name};`
+  },
+  require: [
+    path.resolve(__dirname, 'styleguide/assets/globals.js'),
+  ],
+
 }
---
+++ styleguide/assets/globals.js
@@ -0,0 +1,8 @@
+
+// Here you can define global assets, for example:
+
+// import { Formik } from 'formik'
+// import * as yup from 'yup'
+
+// global.Formik = Formik
+// global.yup = yup
