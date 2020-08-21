1bf41ed7acd4ce5842cc0b013cb68c8a40216e91
Kai Hotz <kaihotz@me.com>
2020-08-22 02:23:32 +0400

    test adjusted

--- src/lib/components/Button/Button.test.js
+++ src/lib/components/Button/Button.test.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 
-import Button from './Button'
+import { Button } from './Button'
 
 describe('<Button />', () => {
   it('should render', () => {
