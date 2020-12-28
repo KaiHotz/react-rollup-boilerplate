917e394afdd60b00ef17f9faeced3dca01d453af
KaiHotz <kaihotz@me.com>
2020-12-28 11:29:06 +0400

    enzyme adapter updated to 17

--- config/setupTests.js
+++ config/setupTests.js
@@ -2,9 +2,9 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import Enzyme from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
+import { configure } from 'enzyme'
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17'
 import '@testing-library/jest-dom/extend-expect'
 
-Enzyme.configure({ adapter: new Adapter() })
+configure({ adapter: new Adapter() })
 
