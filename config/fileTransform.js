d08631f762c15b6932ca24e5797e1260b80c6512
Kai Hotz <kaitobias.hotz@oryxlabs.com>
2022-09-03 22:35:53 +0400

    filetransform for latest jest adjusted

--- config/fileTransform.js
+++ config/fileTransform.js
@@ -8,19 +8,21 @@ module.exports = {
     const assetFilename = JSON.stringify(path.basename(filename));
 
     if (filename.match(/\.svg$/)) {
-      return `module.exports = {
-        __esModule: true,
-        default: ${assetFilename},
-        ReactComponent: (props) => ({
-          $$typeof: Symbol.for('react.element'),
-          type: 'svg',
-          ref: null,
-          key: null,
-          props: Object.assign({}, props, {
-            children: ${assetFilename}
-          })
-        }),
-      };`;
+      return {
+        code: `module.exports = {
+          __esModule: true,
+          default: ${assetFilename},
+          ReactComponent: (props) => ({
+            $$typeof: Symbol.for('react.element'),
+            type: 'svg',
+            ref: null,
+            key: null,
+            props: Object.assign({}, props, {
+              children: ${assetFilename}
+            })
+          }),
+        };`,
+      };
     }
 
     return `module.exports = ${assetFilename};`;
