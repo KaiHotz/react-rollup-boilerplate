4e4e4d9c04a80ffb42e08d0b8a2bde1024cc3e1a
Kai Hotz <kaitobias.hotz@oryxlabs.com>
2022-10-12 20:36:32 +0400

    Rollup update to v3 Code Ql and Ci added to actions

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,6 +1,6 @@
 import { DEFAULT_EXTENSIONS } from '@babel/core';
 import babel from '@rollup/plugin-babel';
-import typescript from 'rollup-plugin-typescript2';
+import rpt2 from '@rollup/plugin-typescript';
 import commonjs from '@rollup/plugin-commonjs';
 import external from 'rollup-plugin-peer-deps-external';
 import postcss from 'rollup-plugin-postcss';
@@ -9,20 +9,22 @@ import url from '@rollup/plugin-url';
 import svgr from '@svgr/rollup';
 import { terser } from 'rollup-plugin-terser';
 import typescriptEngine from 'typescript';
-import pkg from './package.json';
+import pkg from './package.json' assert { type: 'json' };
 
 const config = {
-  input: 'src/index.ts',
+  input: './src/index.ts',
   output: [
     {
       file: pkg.main,
       format: 'cjs',
       exports: 'named',
+      sourcemap: true,
     },
     {
       file: pkg.module,
       format: 'es',
       exports: 'named',
+      sourcemap: true,
     },
   ],
   plugins: [
@@ -33,7 +35,8 @@ const config = {
     external({
       includeDependencies: true,
     }),
-    typescript({
+    rpt2({
+      tsconfig: './tsconfig.json',
       typescript: typescriptEngine,
       include: ['*.js+(|x)', '**/*.js+(|x)'],
       exclude: ['coverage', 'config', 'dist', 'node_modules/**', '*.test.{js+(|x), ts+(|x)}', '**/*.test.{js+(|x), ts+(|x)}'],
@@ -49,6 +52,9 @@ const config = {
     resolve(),
     terser(),
   ],
+  watch: {
+    clearScreen: false,
+  },
 };
 
 export default config;
