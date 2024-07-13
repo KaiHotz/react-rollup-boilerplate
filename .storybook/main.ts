df60f4ae4289da8eb0f07823942101dd7dfd2f73
KaiHotz <kaihotz@me.com>
2024-07-13 15:34:03 +0200

    storybook update

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,36 +3,25 @@ import svgr from 'vite-plugin-svgr';
 import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
-  stories: [
-    '../src/**/*.mdx',
-    '../src/**/*.stories.@(js|jsx|ts|tsx)',
-    '../src/components/**/*.mdx',
-    '../src/components/**/*.story.mdx',
-    '../src/components/**/*.stories.@(js|jsx|ts|tsx)',
-    '../src/components/**/*.story.@(js|jsx|ts|tsx)'
-  ],
-  addons: [
+  'stories': ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+
+  'addons': [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    {
-      name: '@storybook/addon-docs',
-      options: {
-        configureJSX: true,
-        babelOptions: {},
-        sourceLoaderOptions: null,
-        transcludeMarkdown: true
-      }
-    },
-    '@chromatic-com/storybook'
+    '@chromatic-com/storybook',
+    '@storybook/addon-interactions'
   ],
-  framework: {
-    name: '@storybook/react-vite',
-    options: {},
+
+  'framework': {
+    'name': '@storybook/react-vite',
+    'options': {},
   },
-  docs: {},
-  core: {
-    disableTelemetry: true
+  'docs': {},
+  'typescript': {
+    'reactDocgen': 'react-docgen-typescript'
+  },
+  'core': {
+    'disableTelemetry': true
   },
 };
 
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,16 +2,15 @@ import type { Preview } from "@storybook/react";
 
 const preview: Preview = {
   parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
       matchers: {
         color: /(background|color)$/i,
-        date: /Date$/,
+        date: /Date$/i,
       },
     },
   },
 
-  tags: ["autodocs"]
+  tags: ["autodocs", "autodocs"]
 };
 
 export default preview;
