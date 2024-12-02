ad4064ff3ea388acd755878a74bee4f91d731d27
Kai Hotz <kaihotz@me.com>
2024-12-02 09:48:59 +0400

    cjs bundling option removed and buil improved

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -4,11 +4,11 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import url from '@rollup/plugin-url';
+import terser from '@rollup/plugin-terser';
 import svgr from '@svgr/rollup';
 import external from 'rollup-plugin-peer-deps-external';
 import postcss from 'rollup-plugin-postcss';
 import dts from 'rollup-plugin-dts';
-import terser from '@rollup/plugin-terser';
 import typescriptEngine from 'typescript';
 
 const packageJson = JSON.parse(readFileSync('./package.json'));
@@ -17,13 +17,6 @@ export default defineConfig(
   {
     input: './src/index.ts',
     output: [
-      {
-        file: packageJson.main,
-        format: 'cjs',
-        sourcemap: false,
-        exports: 'named',
-        name: packageJson.name,
-      },
       {
         file: packageJson.module,
         format: 'es',
@@ -62,6 +55,7 @@ export default defineConfig(
           '**/*.stories.ts+(|x)',
           '**/*.stories.js+(|x)',
           'setupTests.ts',
+          'vite.config.ts',
           'vitest.config.ts',
         ],
       }),
@@ -71,7 +65,7 @@ export default defineConfig(
     ],
   },
   {
-    input: 'dist/esm/types/src/index.d.ts',
+    input: 'dist/src/index.d.ts',
     output: [{ file: 'dist/index.d.ts', format: 'esm' }],
     external: [/\.(sc|sa|c)ss$/],
     plugins: [dts()],
