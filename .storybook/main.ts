f23bc814540c6414366b8c6e1f020d5616319edd
Kai Hotz <kaihotz@me.com>
2025-07-30 21:25:56 +0400

    dependencies upgraded

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -7,9 +7,8 @@ const config: StorybookConfig = {
 
   'addons': [
     '@storybook/addon-links',
-    '@storybook/addon-essentials',
     '@chromatic-com/storybook',
-    '@storybook/addon-interactions'
+    '@storybook/addon-docs'
   ],
 
   'framework': {
